30b0fc26c19dcce0c18bb8bcb8f8730d61d80154
DeepikaKuppila <catchdeepu.k@gmail.com>
2023-07-31 17:20:12 +0530

    modified

--- lib/util/app_constants.dart
+++ lib/util/app_constants.dart
@@ -5,7 +5,7 @@ class AppConstants {
   static const String appName = 'Manfacturer';
   static const double appVersion = 2.2;
 
-  static const String baseUrl = 'https://connectuz.com/';
+  static const String baseUrl = 'https://connectuz.com';
   static const String configUri = '/api/v1/config';
   static const String loginUri = '/api/v1/auth/vendor/login';
   static const String forgetPasswordUri = '/api/v1/auth/vendor/forgot-password';
